d32434f8c8326d194b736371c348eb6161d14031
Matt <visionsofparadise@pm.me>
2022-09-02 00:08:24 +0100

    simplify, fix validator type

--- src/Item.ts
+++ src/Item.ts
@@ -1,4 +1,3 @@
-import { ValidateFunction } from 'ajv';
 import { get } from 'lodash';
 import pick from 'lodash/pick';
 import { Database } from './Database';
@@ -7,42 +6,26 @@ import { ILogger } from './ILogger';
 export type RequiredKeys<Data extends object, Keys extends keyof Data> = Pick<Data, Keys> & Partial<Omit<Data, Keys>>;
 export type OptionalKeys<Data extends object, Keys extends keyof Data> = Omit<Data, Keys> & Partial<Pick<Data, Keys>>;
 
+export interface IItemConfig<PrimaryKey, Schema> {
+	keys: Array<PrimaryKey>;
+	validator: (data: Schema) => boolean;
+	db: Database;
+	logger?: ILogger;
+	onValidate?: () => Promise<any> | any;
+	onSave?: () => Promise<any> | any;
+	onCreate?: () => Promise<any> | any;
+	onDelete?: () => Promise<any> | any;
+}
+
 export class Item<Schema extends object, PrimaryKey extends keyof Schema> {
-	protected _keys: Array<PrimaryKey>;
-	protected _validator: ValidateFunction<Schema>;
-	protected _db: Database;
-	protected _logger?: ILogger;
 	protected _initial: Schema;
 	protected _current: Schema;
-	protected _onValidate: () => Promise<any> | any;
-	protected _onSave: () => Promise<any> | any;
-	protected _onCreate: () => Promise<any> | any;
-	protected _onDelete: () => Promise<any> | any;
+	config: IItemConfig<PrimaryKey, Schema>;
 
-	constructor(
-		props: Schema,
-		config: {
-			keys: Array<PrimaryKey>;
-			validator: ValidateFunction<Schema>;
-			db: Database;
-			logger?: ILogger;
-			onValidate?: () => Promise<any> | any;
-			onSave?: () => Promise<any> | any;
-			onCreate?: () => Promise<any> | any;
-			onDelete?: () => Promise<any> | any;
-		}
-	) {
+	constructor(props: Schema, config: IItemConfig<PrimaryKey, Schema>) {
 		this._initial = props;
 		this._current = props;
-
-		this._keys = config.keys;
-		this._validator = config.validator;
-		this._db = config.db;
-		this._logger = config.logger;
-		this._onValidate = config.onValidate ? config.onValidate : () => null;
-		this._onSave = config.onSave ? config.onSave : () => null;
-		this._onCreate = config.onCreate ? config.onCreate : () => null;
-		this._onDelete = config.onDelete ? config.onDelete : () => null;
+		this.config = config;
 	}
 
 	public get data() {
@@ -52,7 +35,7 @@ export class Item<Schema extends object, PrimaryKey extends keyof Schema> {
 	public set(data: Partial<Schema>) {
 		this._current = { ...this._current, ...data };
 
-		if (this._logger) this._logger.info(this._current);
+		if (this.config.logger) this.config.logger.info(this._current);
 
 		return;
 	}
@@ -62,15 +45,25 @@ export class Item<Schema extends object, PrimaryKey extends keyof Schema> {
 	}
 
 	public get key() {
-		return pick(this._current, this._keys);
+		return pick(this._current, this.config.keys);
 	}
 
+	public validate = async () => {
+		if (this.config.onValidate) await this.config.onValidate();
+
+		const result = this.config.validator(this._current);
+
+		if (!result) throw new Error('Validation failed');
+
+		return result;
+	};
+
 	public save = async () => {
-		await this._onSave();
+		if (this.config.onSave) await this.config.onSave();
 
 		await this.validate();
 
-		await this._db.put({
+		await this.config.db.put({
 			Item: this._current
 		});
 
@@ -78,12 +71,12 @@ export class Item<Schema extends object, PrimaryKey extends keyof Schema> {
 	};
 
 	public create = async () => {
-		await this._onSave();
-		await this._onCreate();
+		if (this.config.onSave) await this.config.onSave();
+		if (this.config.onCreate) await this.config.onCreate();
 
 		await this.validate();
 
-		await this._db.create(this.key, {
+		await this.config.db.create(this.key, {
 			Item: this._current
 		});
 
@@ -108,7 +101,7 @@ export class Item<Schema extends object, PrimaryKey extends keyof Schema> {
 
 		const UpdateExpression = untrimmedUpdateExpression.slice(0, untrimmedUpdateExpression.length - 2);
 
-		await this._db.update<Schema>({
+		await this.config.db.update<Schema>({
 			Key: this.key,
 			UpdateExpression,
 			ExpressionAttributeValues
@@ -118,7 +111,7 @@ export class Item<Schema extends object, PrimaryKey extends keyof Schema> {
 	};
 
 	public refresh = async () => {
-		const newData = await this._db.get<Schema>({
+		const newData = await this.config.db.get<Schema>({
 			Key: this.key
 		});
 
@@ -126,22 +119,12 @@ export class Item<Schema extends object, PrimaryKey extends keyof Schema> {
 	};
 
 	public delete = async () => {
-		await this._onDelete();
+		if (this.config.onDelete) await this.config.onDelete();
 
-		await this._db.delete({
+		await this.config.db.delete({
 			Key: this.key
 		});
 
 		return;
-	};
-
-	public validate = async () => {
-		await this._onValidate();
-
-		const result = this._validator(this._current);
-
-		if (!result) throw new Error('Validation failed');
-
-		return result;
 	};
 }
